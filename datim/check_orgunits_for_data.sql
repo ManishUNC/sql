10887b70352b78515919a8cc0dc45c459958635c
Michael Mwebaze <michael.mwebaze@gmail.com>
2017-02-21 10:06:55 -0500

    updated sql queries

--- datim/check_orgunits_for_data.sql
+++ datim/check_orgunits_for_data.sql
@@ -1,8 +1,7 @@
 ﻿-- includes dataset but they get repeated 
 
-SELECT ou.uid, ou.name as orgname, ds.shortname as datasetname, de.uid as dataelement, de.code dataelementcode, ps.iso as period, co.uid as disaggregate, gh.uid as mechanism , dv.value, dv.lastupdated FROM datavalue dv 
--- checks for data for a particular org units includes dataset (datasets get repeated)
-
+SELECT ou.uid, ou.name as orgname, ds.shortname as datasetname, de.uid as dataelement, de.code dataelementcode, 
+ps.iso as period, co.uid as disaggregate, gh.uid as mechanism , dv.value, dv.lastupdated FROM datavalue dv 
 INNER JOIN organisationunit ou on (dv.sourceid = ou.organisationunitid) 
 INNER JOIN _periodstructure ps on (dv.periodid = ps.periodid) 
 INNER JOIN dataelement de on (de.dataelementid = dv.dataelementid) 
@@ -12,8 +11,7 @@ INNER JOIN datasetsource dss on (dss.sourceid = ou.organisationunitid)
 INNER JOIN dataset ds on (ds.datasetid = dss.datasetid)
 INNER JOIN dataset ds on (ds.datasetid = dm.datasetid)
 INNER JOIN datasetmembers dm on (dm.dataelementid = de.dataelementid) 
-WHERE ou.uid = 'WJlPkJdb5xb' OR ou.uid = 'JuKf6u400L5' OR ou.uid = 'HLR8kQKGLzU' OR ou.uid = 'ltSETzFTBXI' 
-OR ou.uid = 'vncLNiGh5yn' OR ou.uid = 'bbFetbhJ297' OR ou.uid = 'V6mRoVkM4Ru'
+WHERE ou.uid = 'RnVtVN0MtKZ'
 
 -- checks for data for a particular org unit and doesn't include dataset
 
@@ -25,11 +23,22 @@ WHERE ou.uid = 'WN9Ys0h43os'
 
 -- gets data for Uganda for particular mechanisms
 
-SELECT ou.uid, ou.name as orgname, de.uid as dataelement, de.code dataelementcode, ps.iso as period, co.uid as disaggregate, gh.uid as mechanism , dv.value, dv.lastupdated FROM datavalue dv 
+SELECT ou.uid, ou.name as orgname, de.uid as dataelement, de.code dataelementcode, ps.iso as period, co.uid as disaggregate, gh.uid as mechanism_uid , gh.name as mech_name, dv.value, dv.lastupdated FROM datavalue dv 
 INNER JOIN organisationunit ou on (dv.sourceid = ou.organisationunitid) 
 INNER JOIN _periodstructure ps on (dv.periodid = ps.periodid) 
 INNER JOIN dataelement de on (de.dataelementid = dv.dataelementid) 
 INNER JOIN categoryoptioncombo co on (co.categoryoptioncomboid = dv.categoryoptioncomboid) 
 INNER JOIN categoryoptioncombo gh on (gh.categoryoptioncomboid = dv.attributeoptioncomboid)
 INNER JOIN datasetsource dss on (dss.sourceid = ou.organisationunitid)
-WHERE ou.uid = 'WN9Ys0h43os' AND (gh.uid = 'b3BshzziY5r' OR gh.uid = 'rpFB1EnTc8d')
+WHERE ou.uid IN ('EicUMzaSLtP','TmAPlI4DVsF','FmblH9NgMkV') AND ps.iso IN ('2016OCT', '2015OCT')
+
+-- ticket 6636
+
+SELECT ou.uid, ou.name as orgname, de.uid as dataelement, de.code dataelementcode, ps.iso as period, co.uid as disaggregate, gh.uid as mechanism_uid , gh.name as mech_name, dv.value, dv.lastupdated FROM datavalue dv 
+INNER JOIN organisationunit ou on (dv.sourceid = ou.organisationunitid) 
+INNER JOIN _periodstructure ps on (dv.periodid = ps.periodid) 
+INNER JOIN dataelement de on (de.dataelementid = dv.dataelementid) 
+INNER JOIN categoryoptioncombo co on (co.categoryoptioncomboid = dv.categoryoptioncomboid) 
+INNER JOIN categoryoptioncombo gh on (gh.categoryoptioncomboid = dv.attributeoptioncomboid)
+INNER JOIN datasetsource dss on (dss.sourceid = ou.organisationunitid)
+WHERE ou.uid IN ('EicUMzaSLtP','TmAPlI4DVsF','FmblH9NgMkV') AND  de.uid IN ('f9BaCsI9PGn','NUdcIMK4Peq')
